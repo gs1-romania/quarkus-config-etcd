f6dc4913266bfbbdd195ba8df1e6257ce9bf95b8
Savu Liviu Gabriel <liviu.savu@gs1.ro>
2022-07-07 16:47:40 +0300

    remove io.grpc from jetcd and added quarkus grpc

--- runtime/src/main/java/ro/gs1/quarkus/config/etcd/runtime/ReflectionConfiguration.java
+++
@@ -1,9 +0,0 @@
-package ro.gs1.quarkus.config.etcd.runtime;
-
-import io.grpc.internal.DnsNameResolver;
-import io.grpc.internal.DnsNameResolverProvider;
-import io.quarkus.runtime.annotations.RegisterForReflection;
-
-@RegisterForReflection(targets = { DnsNameResolver.class, DnsNameResolverProvider.class })
-public class ReflectionConfiguration {
-}
